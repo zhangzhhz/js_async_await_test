4673f5d489c059f57e48ba1390b421d358a59d9b
ZH Zhang <zhangzhhz@gmail.com>
2020-01-16 16:18:24 +0000

    add

--- bitsrc.io/test01.js
+++ bitsrc.io/test01.js
@@ -1,16 +1,53 @@
-function asyncThing (value) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve(value), 100)
-  })
+/*
+* Print numbers sequentially.
+*/
+
+const wait = (i, ms) => new Promise(resolve => setTimeout(() => resolve(i), ms));
+
+// Implementation One (Using for-loop)
+const printNumbers = () => new Promise((resolve) => {
+  let pr = Promise.resolve(0);
+  for (let i = 1; i <= 10; i += 1) {
+    pr = pr.then((val) => {
+      console.log(val);
+      return wait(i, Math.random() * 1000);
+    });
+  }
+  resolve(pr);
+});
+
+// Implementation Two (Using Recursion)
+
+const printNumbersRecursive = () => {
+  return Promise.resolve(0).then(function processResult(i) {
+
+    if (i === 10) {
+      return undefined;
+    }
+
+    return wait(i, Math.random() * 1000).then((val) => {
+      console.log(val);
+      return processResult(i + 1);
+    });
+  });
+};
+
+async function printNumbersUsingAsync(){
+  for (let i = 0; i < 10; i++) {
+    await wait(i, Math.random() * 1000);
+    console.log(i);
+  }
+  console.log('=======');
 }
 
-async function main () {
-  return [1,2,3,4].map(async (value) => {
-    const v = await asyncThing(value)
-    return v * 2
-  })
+// Uncomment one at a time to print
+async function run() { 
+  console.log("***** printNumbers *****");
+  await printNumbers();
+  console.log("***** printNumbersRecursive *****");
+  await printNumbersRecursive();
+  console.log("***** printNumbersUsingAsync *****");
+  await printNumbersUsingAsync().then(console.log);
 }
 
-main()
-  .then(v => console.log(v))
-  .catch(err => console.error(err))
+run();
